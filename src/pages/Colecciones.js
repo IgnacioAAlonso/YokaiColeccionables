4f13b92f13f5e222919bc95eac1e1646ba049513
ignacio alonso <ignacioanacional@gmail.com>
2021-12-09 16:37:53 -0300

    Commit del objeto Item

--- src/pages/Colecciones.js
+++ src/pages/Colecciones.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import ItemListContainer from '../components/ItemListContainer'
+import Item from '../components/Item/Item'
 
 const Colecciones = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
-            <ItemListContainer textoTitulo={"Colecciones"}></ItemListContainer>
+            <Item/>
         </div>
     )
 }
