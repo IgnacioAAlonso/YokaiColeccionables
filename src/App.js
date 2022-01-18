1472abfdc200f225b6b9ad7d2d07a4595ab1e497
ignacio alonso <ignacioanacional@gmail.com>
2022-01-18 19:50:19 -0300

    Commit del arreglo en el inicio

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes  >
-          <Route path='/' element={<Home />} />
+          <Route path='/YokaiColeccionables/' element={<Home />} />
           <Route path='/colecciones' element={<Colecciones />} />
           <Route path='/category/:id' element={<Colecciones />} />
           <Route path='/item/:id' element={<ItemDetailContainer />} />
